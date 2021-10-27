5e6f19a31105bd97ec256daefbde4f9d88f417d0
zbuster05 <zakary.saya@gmail.com>
2021-10-27 00:02:35 -0700

    Adding wishlist items before I sleep and forget

--- addusers.js
+++ addusers.js
@@ -6,6 +6,11 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
+// TODO run expirements
+// 1. Figure out if perms are all no by default or if they have to be set
+// 2. Attempt to recreate table not found crashing
+
+
 rl.question('Admin password (if one specified otherwise leave blank)', admin_password => {
     r.connect( {host:'localhost', port: 28015, user: "admin", password: admin_password}, function(err, conn) {
 	rl.question('New user username: '){}
